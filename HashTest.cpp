48b528c4601c343942be34598816be7d3472626b
ItsYaBoiFrankie <31866615+ItsYaBoiFrankie@users.noreply.github.com>
2017-12-02 00:20:44 -0700

    Add files via upload
    Can't remember if I edited any of the bTREE files, but I added a substantial amount into HashTest. Just need to fix recursion on hashing and comparison functions,

--- HashTest.cpp
+++ HashTest.cpp
@@ -1,1 +1,103 @@
-//trash file
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <string>
+#include <vector>
+
+#include "bTREE.h"
+
+using namespace std;
+
+int hashFileOps = 0;
+
+bool compare(bTREE* uno, bTREE* dos)
+{
+	int oddBalls = 0; //tmp Var to hold timeStamps of nodes that differ.
+	if (uno->root->word != dos->root->word)
+	{
+		hashFileOps++;
+		//Navigate through each node comparing root->word, grab their timeStamp values and print them.
+		cout << "Trees differ at time stamps: " << oddBalls;
+		cout << endl;
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+string myhash(string data)
+{
+	{
+		//Reverses the spelling of the word, reassigns certain characters to new ones and pushes it into a vector, then returns the values in the vector back to a string in index order.
+		for (int i = data.length(); i >= 0; i--)
+		{
+			hashFileOps++;
+			replace(data.begin(), data.end(), 'a', '*');
+			replace(data.begin(), data.end(), 'e', '|');
+			replace(data.begin(), data.end(), 'i', '~');
+			replace(data.begin(), data.end(), 'o', '9');
+			replace(data.begin(), data.end(), 't', '7');
+			replace(data.begin(), data.end(), 'n', '5');
+			replace(data.begin(), data.end(), 's', '3');
+			replace(data.begin(), data.end(), 'h', '1');
+		}
+		return data;
+	}
+}
+
+//The function only hashes the root node, it does not contine on to hash Kappa. Something with my recursion is wrong.
+int doMyHash(treeNode* node)
+{
+	if (node->leaf == false)
+	{
+		hashFileOps++;
+		node->word = myhash(node->left->word) + myhash(node->right->word);
+		return 0;
+	}
+	else
+	{
+		hashFileOps++;
+		doMyHash(node->left);
+		hashFileOps++;
+		doMyHash(node->right);
+	}
+}
+
+int main()
+{
+	bTREE tree = bTREE(); //create bTree.
+	bTREE pine = bTREE();
+	int input = -1; //control var for user input.
+	string wordData;; //tmp var for userInput.
+
+	tree.insert("GitGudKid");
+	cout << "It takes this many ops to insert a new node: " << tree.ops + hashFileOps << endl << endl;
+	tree.insert("Kappa");
+	tree.insert("NetFlixAndChill");
+	tree.insert("ILikeBigButts");
+	tree.insert("Durka");
+
+	tree.display(cout);
+
+	doMyHash(tree.root);
+	cout << "How many ops to hash one of our trees: " << hashFileOps + tree.ops << endl;
+
+	pine.insert("GitGudKid");
+	pine.insert("Kappa");
+	pine.insert("NetFlixAndChill");
+	pine.insert("ILikeBigButts");
+	pine.insert("Murica"); //Here is where it differs.
+
+	doMyHash(pine.root);
+	pine.display(cout);
+
+	cout << "How many ops to compare our trees : " << hashFileOps + tree.ops + pine.ops + 1 << endl;
+	compare(&tree, &pine);
+	cout << "How many ops to determine where one of our trees differ: " << hashFileOps + tree.ops + pine.ops + 1 << endl;
+	
+	system("PAUSE");
+	return 0;
+}
--- bTREETest.cpp
+++ bTREETest.cpp
@@ -1,74 +1,74 @@
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
-
-#include "pMT.h"
-#include "bTREE.h"
-
-using namespace std;
-
-int main()
-{
-	bTREE tree = bTREE(); //create bTree.
-	int input = -1; //control var for user input.
-	string wordData; //tmp var for userInput.
-
-	while (input != 0)
-	{
-		cout << "Please select an option for your bTREE:" << endl << endl;
-		cout << "1. Count nodes." << endl;
-		cout << "2. Insert a new node." << endl;
-		cout << "3. Find a node with it's string." << endl;
-		cout << "4. Locate the path to a node via it's hash." << endl;
-		cout << "5. Report the amount of total Operations so far." << endl;
-		cout << "0. Exit." << endl;
-		cin >> input;
-		cout << endl;
-
-		if (input != 0)
-		{
-			tree.ops++;
-			switch (input)
-			{
-			case 1:
-				tree.ops++;
-				//this works
-				cout << tree.numberOfNodes(tree.root) << endl;
-				break;
-			case 2:
-				tree.ops++;
-				//this no worky
-				cout << "Please enter a string to insert into the new node:" << endl;
-				cin >> wordData;
-				tree.insert(wordData);
-				//dont delete this function below from case 2 
-				tree.display(cout);
-				break;
-			case 3:
-				tree.ops++;
-				//not tested yet.
-				cout << "Enter a string to search for:" << endl;
-				cin >> wordData;
-				cout << tree.callerfind(wordData) << endl;
-				break;
-			case 4:
-				tree.ops++;
-				//not tested yet.
-				cout << "Enter a hash to search the tree for:" << endl;
-				cin >> wordData;
-				cout << tree.locate(wordData) << endl;
-				break;
-			case 5:
-				tree.ops++;
-				//this works.
-				cout << "Amount of operations so far: " << tree.ops << endl;
-				break;
-			default:
-				tree.ops++;
-				//this works.
-				cout << "Not a Valid Option." << endl;
-			}
-		}
-	}
-	return 0;
-}
+//#include <stdio.h>
+//#include <iostream>
+//#include <fstream>
+//
+//#include "pMT.h"
+//#include "bTREE.h"
+//
+//using namespace std;
+//
+//int main()
+//{
+//	bTREE tree = bTREE(); //create bTree.
+//	int input = -1; //control var for user input.
+//	string wordData; //tmp var for userInput.
+//
+//	while (input != 0)
+//	{
+//		cout << "Please select an option for your bTREE:" << endl << endl;
+//		cout << "1. Count nodes." << endl;
+//		cout << "2. Insert a new node." << endl;
+//		cout << "3. Find a node with it's string." << endl;
+//		cout << "4. Locate the path to a node via it's hash." << endl;
+//		cout << "5. Report the amount of total Operations so far." << endl;
+//		cout << "0. Exit." << endl;
+//		cin >> input;
+//		cout << endl;
+//
+//		if (input != 0)
+//		{
+//			tree.ops++;
+//			switch (input)
+//			{
+//			case 1:
+//				tree.ops++;
+//				//this works
+//				cout << tree.numberOfNodes(tree.root) << endl;
+//				break;
+//			case 2:
+//				tree.ops++;
+//				//this no worky
+//				cout << "Please enter a string to insert into the new node:" << endl;
+//				cin >> wordData;
+//				tree.insert(wordData);
+//				//dont delete this function below from case 2 
+//				tree.display(cout);
+//				break;
+//			case 3:
+//				tree.ops++;
+//				//not tested yet.
+//				cout << "Enter a string to search for:" << endl;
+//				cin >> wordData;
+//				cout << tree.callerfind(wordData) << endl;
+//				break;
+//			case 4:
+//				tree.ops++;
+//				//not tested yet.
+//				cout << "Enter a hash to search the tree for:" << endl;
+//				cin >> wordData;
+//				cout << tree.locate(wordData) << endl;
+//				break;
+//			case 5:
+//				tree.ops++;
+//				//this works.
+//				cout << "Amount of operations so far: " << tree.ops << endl;
+//				break;
+//			default:
+//				tree.ops++;
+//				//this works.
+//				cout << "Not a Valid Option." << endl;
+//			}
+//		}
+//	}
+//	return 0;
+//}
